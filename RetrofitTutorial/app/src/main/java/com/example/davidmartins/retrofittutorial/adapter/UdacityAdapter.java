08a97aac3d4c78d75ad1056b34c028ba76786523
David Martins <d.martins1988@gmail.com>
2016-04-21 23:36:14 +0100

    add butterknife library to the project

--- RetrofitTutorial/app/src/main/java/com/example/davidmartins/retrofittutorial/adapter/UdacityAdapter.java
+++ RetrofitTutorial/app/src/main/java/com/example/davidmartins/retrofittutorial/adapter/UdacityAdapter.java
@@ -4,6 +4,12 @@ package com.example.davidmartins.retrofittutorial.adapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.davidmartins.retrofittutorial.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by davidmartins on 21/04/16.
@@ -29,8 +35,15 @@ public class UdacityAdapter extends RecyclerView.Adapter<UdacityAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+
+        @Bind(R.id.course_textView)
+        TextView courserTextView;
+        @Bind(R.id.teacher_textView)
+        TextView teacherTextView;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
